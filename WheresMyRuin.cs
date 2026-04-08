6663ca2387f0a1f0175469fa7b58df805d4e2736
doubleespressobro <doubleespressobro@gmail.com>
2026-04-08 13:43:48 -0600

    Add project files.

---
+++ WheresMyRuin.cs
@@ -0,0 +1,122 @@
+﻿using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using SharpDX;
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using Vector2 = System.Numerics.Vector2;
+
+namespace WheresMyRuin;
+
+public class WheresMyRuin : BaseSettingsPlugin<WheresMyRuinSettings>
+{
+    public override bool Initialise()
+    {
+        return true;
+    }
+
+    public void Quit()
+    {
+        var code = CommandHandler.KillTCPConnectionForProcess(GameController.Window.Process.Id);
+
+        if (code == 317)
+        {
+            LogError("Run program from admin or set compatibility to Run as administrator");
+        }
+    }
+
+    public override Job Tick()
+    {
+        var player = GameController.Game.IngameState.Data.LocalPlayer;
+        if (player == null)
+            return null;
+
+        var ruinBuff = player.GetComponent<Buffs>()?.BuffsList.FirstOrDefault(b => b.Name == "gauntlet_ruinbuff");
+        if (ruinBuff == null)
+            return null;
+
+        if (ruinBuff.BuffCharges >= Settings.RuinCharges)
+            Quit();
+
+        return null;
+    }
+}
+
+public static partial class CommandHandler
+    {
+        public static int KillTCPConnectionForProcess(int ProcessId)
+        {
+            MibTcprowOwnerPid[] table;
+            var afInet = 2;
+            var buffSize = 0;
+            var ret = GetExtendedTcpTable(IntPtr.Zero, ref buffSize, true, afInet, TcpTableClass.TcpTableOwnerPidAll);
+            var buffTable = Marshal.AllocHGlobal(buffSize);
+            try
+            {
+                ret = GetExtendedTcpTable(buffTable, ref buffSize, true, afInet, TcpTableClass.TcpTableOwnerPidAll);
+                if (ret != 0)
+                    return 0;
+                var tab = (MibTcptableOwnerPid)Marshal.PtrToStructure(buffTable, typeof(MibTcptableOwnerPid));
+                var rowPtr = (IntPtr)((long)buffTable + Marshal.SizeOf(tab.dwNumEntries));
+                table = new MibTcprowOwnerPid[tab.dwNumEntries];
+                for (var i = 0; i < tab.dwNumEntries; i++)
+                {
+                    var tcpRow = (MibTcprowOwnerPid)Marshal.PtrToStructure(rowPtr, typeof(MibTcprowOwnerPid));
+                    table[i] = tcpRow;
+                    rowPtr = (IntPtr)((long)rowPtr + Marshal.SizeOf(tcpRow));
+
+                }
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffTable);
+            }
+
+            //Kill Path Connection
+            var PathConnection = table.FirstOrDefault(t => t.owningPid == ProcessId);
+            PathConnection.state = 12;
+            var ptr = Marshal.AllocCoTaskMem(Marshal.SizeOf(PathConnection));
+            Marshal.StructureToPtr(PathConnection, ptr, false);
+            var tcpEntry = SetTcpEntry(ptr);
+            return tcpEntry;
+        }
+
+        [DllImport("iphlpapi.dll", SetLastError = true)]
+        private static extern uint GetExtendedTcpTable(IntPtr pTcpTable, ref int dwOutBufLen, bool sort, int ipVersion, TcpTableClass tblClass, uint reserved = 0);
+
+        [DllImport("iphlpapi.dll")]
+        private static extern int SetTcpEntry(IntPtr pTcprow);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MibTcprowOwnerPid
+        {
+            public uint state;
+            public uint localAddr;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] public byte[] localPort;
+            public uint remoteAddr;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] public byte[] remotePort;
+            public uint owningPid;
+
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MibTcptableOwnerPid
+        {
+            public uint dwNumEntries;
+            private readonly MibTcprowOwnerPid table;
+        }
+
+        private enum TcpTableClass
+        {
+            TcpTableBasicListener,
+            TcpTableBasicConnections,
+            TcpTableBasicAll,
+            TcpTableOwnerPidListener,
+            TcpTableOwnerPidConnections,
+            TcpTableOwnerPidAll,
+            TcpTableOwnerModuleListener,
+            TcpTableOwnerModuleConnections,
+            TcpTableOwnerModuleAll
+        }
+    }
---
+++ WheresMyRuinSettings.cs
@@ -0,0 +1,11 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+
+namespace WheresMyRuin;
+
+public class WheresMyRuinSettings : ISettings
+{
+    //Mandatory setting to allow enabling/disabling your plugin
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    public RangeNode<int> RuinCharges { get; set; } = new RangeNode<int>(4, 1, 7);
+}
